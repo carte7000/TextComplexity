b47d6f986d043b405d284ab58f1fb3db605dc876
Simon Boissonneault-Robert <simrob7000@gmail.com>
2015-11-28 20:52:54 -0500

    Added fileFetcher

--- tp3/src/main/java/org/ift7022/tp3/App.java
+++ tp3/src/main/java/org/ift7022/tp3/App.java
@@ -25,8 +25,10 @@ public class App
     	Parser parser = new NgramParser();
     	PosTagger tagger = new GoogleStylePosTagger(new UniversalPosTagger(openNlpTagger, FileFetcher.MAP));
     	TopNgramContainer topNgramContainer = new TopNgramContainer();
-    	NgramRepository ngramRepository = new RedisNgramRepository();
-    	NgramRepository posRepository = new RedisNgramRepository("POS");
+    	//NgramRepository ngramRepository = new RedisNgramRepository();
+    	//NgramRepository posRepository = new RedisNgramRepository("POS");
+    	NgramRepository ngramRepository = new RedisNgramRepository(FileFetcher.COMPRESSED_1GRAM, parser);
+    	NgramRepository posRepository = new RedisNgramRepository(FileFetcher.COMPRESSED, parser, "POS");
     	Text text = new Text(topNgramContainer, ngramRepository, posRepository, tagger);
     	text.updateText("I am a blue and grey dog and i like it. House House House House screamed Bob. 203 want Bob and 202. Want want want a do.");    	
         System.out.println(text.getPosPerplexity());
